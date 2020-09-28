d671f1ea91fad1da1ad33d0a8595bf1a24d4d989
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-28 14:41:22 +0530

    Updates

---
+++ precache-manifest.4e42979b1bf61280572fad023a7a9377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34830a946b02397c90c1aa114483665e",
+    "url": "/lightout/index.html"
+  },
+  {
+    "revision": "984c9df54b71f5b4b91c",
+    "url": "/lightout/static/css/main.3c8e6ee3.chunk.css"
+  },
+  {
+    "revision": "55d3b61f94c09dce4753",
+    "url": "/lightout/static/js/2.202eafd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lightout/static/js/2.202eafd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984c9df54b71f5b4b91c",
+    "url": "/lightout/static/js/main.a2012216.chunk.js"
+  },
+  {
+    "revision": "c3739cc37193550c7393",
+    "url": "/lightout/static/js/runtime-main.34f96ee4.js"
+  }
+]);
